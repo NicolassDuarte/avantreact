ff811e3431f09f311800056cd2acfe5fd51c1076
Ray Martins <rhaymarthins@gmail.com>
2025-07-15 21:19:10 -0300

    Implementação da pagina de Login com sua estilização

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { Router } from 'react-router-dom'
 import './App.css'
 import { BrowserRouter, Routes, Route } from "react-router-dom"
-import Login from "./pages/Login"
+import Login from "./pages/Login/Login"
 import Perfil from "./pages/Perfil"
 import Anuncios from "./pages/Anuncios"
 import Home from "./pages/Home"
@@ -11,23 +11,23 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
   return (
-    
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />}/>
-          <Route path="/login" element={<Login />}/>
-          <Route path="/perfil" element={<Perfil />}/>
-          <Route path="/anuncios" element={<Anuncios />}/>
-          <Route path="/populares" element={<Populares />}/>
-        </Routes>
-      </BrowserRouter>
-        // {/* <Sessionnav/>
-        // <section className='card-troca'></section>
-        // <section className='card-sobre'></section>
-        // <section className='card-rotation'></section>
-        // <section className='card-comentarios'></section>
-        // <Footer/> */}
-        
+
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/perfil" element={<Perfil />} />
+        <Route path="/anuncios" element={<Anuncios />} />
+        <Route path="/populares" element={<Populares />} />
+      </Routes>
+    </BrowserRouter>
+    // {/* <Sessionnav/>
+    // <section className='card-troca'></section>
+    // <section className='card-sobre'></section>
+    // <section className='card-rotation'></section>
+    // <section className='card-comentarios'></section>
+    // <Footer/> */}
+
   )
 }
 
