270063cc2384fdbd2083f89a22191ffe420228dd
devjoaol <124259261+devjoaol@users.noreply.github.com>
2025-08-22 15:52:34 -0300

    feat: model categoria

---
+++ backend/src/models/categoriaModel.js
@@ -0,0 +1,54 @@
+const prisma = require("../prisma");
+
+const getAllCategorias = async () => {
+  return await prisma.categoria.findMany({
+    orderBy: {
+      nome: "asc",
+    },
+  });
+};
+
+const getCategoriaById = async (id_categoria) => {
+  return await prisma.categoria.findUnique({
+    where: {
+      id_categoria: id_categoria,
+    },
+  });
+};
+
+const addCategoria = async (nome) => {
+  return await prisma.categoria.create({
+    data: {
+      nome: nome,
+    },
+  });
+};
+
+const deleteCategoria = async (id_categoria) => {
+  const categoria = getCategoriaById(id_categoria);
+  if (!categoria) {
+    throw new Error("Categoria não encontrada");
+  }
+  return await prisma.categoria.delete({
+    where: {
+      id_categoria: id_categoria,
+    },
+  });
+};
+
+const updateCategoria = async (id_categoria, nome, itens) => {
+  const categoria = getCategoriaById(id_categoria);
+  if (!categoria) {
+    throw new Error("Categoria não encontrada");
+  }
+
+  return await prisma.categoria.update({
+    where: {
+      id_categoria: id_categoria,
+    },
+    data: {
+      nome: nome,
+      itens: itens,
+    },
+  });
+};
